0ddf46951fefb8d4feeeb2c067b098ad68144802
Lan Pavletič <lan@superdatascience.com>
2021-03-12 16:00:23 +0100

    init

---
+++ imports/ui/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+  </div>
+);
